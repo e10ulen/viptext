9ac16f0f8518d6835cd206110e75c5752467ecb4
e10ulen <slcb.sena@gmail.com>
2021-10-19 11:17:53 +0900

    [Commit]2021/10/19 11:17 file save to sjis & file encoding sjis to utf8

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/e10ulen/viptext
 go 1.17
 
 require (
-	github.com/comail/colog v0.0.0-20160416085026-fba8e7b1f46c // indirect
-
+	github.com/comail/colog v0.0.0-20160416085026-fba8e7b1f46c
+	golang.org/x/text v0.3.7
 )
--- main.go
+++ main.go
@@ -1,24 +1,57 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"bufio"
+	"fmt"
+	"io"
+
 	"github.com/comail/colog"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 	colog.Register()
 	//	ファイル取得して、ファイル書き出し処理.
+	resp, _ := http.Get("http://dawnlight.ovh/viptext/#ui-tabs-2")
+	filename := "viptext.sjis.html"
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	println(string(body))
+	writeBody := []byte(body)
+	err := ioutil.WriteFile(filename, writeBody, 0664)
+	if err != nil {
+		fmt.Println(err)
+	}
+	sjisFile, err := os.Open("./viptext.sjis.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sjisFile.Close()
 
-	/*
-		resp, _ := http.Get("http://dawnlight.ovh/viptext/#ui-tabs-2")
-		filename := "vip.txt"
-		body, _ := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
+	// ShiftJISのデコーダーを噛ませたReaderを作成する
+	reader := transform.NewReader(sjisFile, japanese.ShiftJIS.NewDecoder())
 
-		println(string(body))
-		writeBody := []byte(body)
-		err := ioutil.WriteFile(filename, writeBody, 0664)
-		if err != nil {
-			fmt.Println(err)
-		}
-	*/
+	// 書き込み先ファイルを用意
+	utf8File, err := os.Create("./viptext.uft8.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer utf8File.Close()
+
+	// 書き込み
+	tee := io.TeeReader(reader, utf8File)
+	s := bufio.NewScanner(tee)
+	for s.Scan() {
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("done")
+
 }
