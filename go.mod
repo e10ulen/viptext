c23ce472f19f9bc94854921caf52d6a0833fafd2
e10ulen <slcb.sena@gmail.com>
2021-10-19 17:40:39 +0900

    [Commit]2021/10/19 17:40 sanitize file

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/e10ulen/viptext
 go 1.17
 
 require (
-	github.com/comail/colog v0.0.0-20160416085026-fba8e7b1f46c
-	golang.org/x/text v0.3.7
+	github.com/microcosm-cc/bluemonday v1.0.16
+	golang.org/x/text v0.3.6
+)
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 )
--- main.go
+++ main.go
@@ -10,19 +10,20 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/comail/colog"
+	"github.com/microcosm-cc/bluemonday"
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
 )
 
+const url = "http://dawnlight.ovh/test/read.cgi/viptext/1597046459"
+
 func main() {
-	colog.Register()
+
 	//	ファイル取得して、ファイル書き出し処理.
-	resp, _ := http.Get("http://dawnlight.ovh/viptext/#ui-tabs-2")
+	resp, _ := http.Get(url)
 	filename := "viptext.sjis.html"
 	body, _ := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
-	println(string(body))
 	writeBody := []byte(body)
 	err := ioutil.WriteFile(filename, writeBody, 0664)
 	if err != nil {
@@ -54,4 +55,54 @@ func main() {
 	}
 	log.Println("done")
 
+	//	ここから先でパーサーを通す事
+	//	File Open
+	file, err := os.OpenFile("./viptext.uft8.html", os.O_RDWR, 0664) // For read access.
+	fmt.Println("file Opens!!!")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	policy := bluemonday.NewPolicy()
+
+	policy.AllowElements("dd")
+	doc := policy.SanitizeReader(file)
+	fmt.Println(doc)
+	// 書き込み先ファイルを用意
+	sanitizeFile, err := os.Create("./viptext.sinitize.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sanitizeFile.Close()
+
+	// 書き込み
+	teesanitize := io.TeeReader(doc, sanitizeFile)
+	docs := bufio.NewScanner(teesanitize)
+	for docs.Scan() {
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Sanitize done")
+	/*
+	   //	goquery
+	   	doc, err := goquery.NewDocumentFromReader(file)
+
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	doc.Find("table > dl > dd").Each(func(i int, s *goquery.Selection) {
+	   		band := s.Find("a").Text()
+	   		urls, _ := s.Attr("href")
+	   		fmt.Println("URLs: %s", urls)
+	   		fmt.Println("SiteTitle: %s", band)
+
+	   	})
+	   //	html2text
+	   		text, err := html2text.FromString(string(file), html2text.Options{PrettyTables: true})
+	   		if err != nil {
+	   			panic(err)
+	   		}
+	   		fmt.Println(text)
+	*/
 }
